83ae7d995cb796657ab9af515b2281929be7b6c0
greensky <102775197+greencase3@users.noreply.github.com>
2023-04-19 16:42:55 +0900

    Update Hop.lua

--- Hop.lua
+++ Hop.lua
@@ -1,19 +1,4 @@
 util.keep_running()require"natives-1627063482"
- 
-function playerActionsSetup(she)
- ped = PLAYER.GET_PLAYER_PED_SCRIPT_INDEX(she)
-menu.toggle_loop(menu.player_root(she),"Hop",{},"",function()
-  if not ENTITY.IS_ENTITY_IN_AIR(ped) then
-    memory.write_int(memory.script_global(4521801)+935,NETWORK.GET_NETWORK_TIME())
- local pos = ENTITY.GET_ENTITY_COORDS(ped)
-  menu.trigger_commands("autoheal " .. players.get_name(she).." on")
-    FIRE.ADD_EXPLOSION(pos.x, pos.y, pos.z - 1.0, 13, 5.0, false, true, 0.3, true)
-  else
-    menu.trigger_commands("autoheal " .. players.get_name(she).." off")  
-  end
-end)
-end players.on_join(playerActionsSetup)players.dispatch_on_join()
-
 
 menu.toggle_loop(menu.my_root(),"Hop-Self",{},"",function()
   if not ENTITY.IS_ENTITY_IN_AIR(players.user_ped()) then
@@ -27,15 +12,44 @@ menu.toggle_loop(menu.my_root(),"Hop-Self",{},"",function()
     menu.trigger_commands("demigodmode off")     
   end
 end)
+
+function playerActionsSetup(she)
+
+ ped = PLAYER.GET_PLAYER_PED_SCRIPT_INDEX(she)
+menu.toggle_loop(menu.player_root(she),"Hop",{},"",function()
+  if not ENTITY.IS_ENTITY_IN_AIR(ped) then
+    memory.write_int(memory.script_global(4521801)+935,NETWORK.GET_NETWORK_TIME())
+ local pos = ENTITY.GET_ENTITY_COORDS(ped)
+  menu.trigger_commands("autoheal " .. players.get_name(she).." on")
+    FIRE.ADD_EXPLOSION(pos.x, pos.y, pos.z - 1.0, 13, 5.0, false, true, 0.3, true)
+  else
+    menu.trigger_commands("autoheal " .. players.get_name(she).." off")  
+  end
+end)
+
+menu.toggle_loop(menu.my_root(),"height[her]",{},"",function()
+  local pos = ENTITY.GET_ENTITY_COORDS(ped)
+
+   pos.z = math.floor(math.abs(pos.z * 3.28084 / 3) + 0.5)
+ if pos2.z ~= pos.z then
+  util.yield(500)
+util.toast("Z/ "..pos.z)
+  end
+  pos2 = pos
+end)
+
+end players.on_join(playerActionsSetup)players.dispatch_on_join()
+
+
 pos2 = ENTITY.GET_ENTITY_COORDS(players.user_ped())
 
-menu.toggle_loop(menu.my_root(),"print",{},"",function()
+menu.toggle_loop(menu.my_root(),"height",{},"",function()
 
   local pos = ENTITY.GET_ENTITY_COORDS(players.user_ped())
 
    pos.z = math.floor(math.abs(pos.z * 3.28084 / 3) + 0.5)
  if pos2.z ~= pos.z then
-  util.yield(1000)
+  util.yield(500)
 util.toast("Z/ "..pos.z)
   end
   pos2 = pos
